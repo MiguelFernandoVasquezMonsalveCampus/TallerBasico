4b16a60f66014af2765de6c1c07eca0e76875bf6
miguel vasquez <miguelvasquezf8@gmail.com>
2023-03-08 13:16:49 -0500

    feat: :zap: Javascript 2 charAt
    metodo de javascript de charAt para buscar una letra en una string con el index seleccionado
    
    by Miguel Vasquez

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 //metodos de string
 
 let parrafo = 'Hello world';
-
-
 let largo = parrafo.length;
-
 console.log(`Largo del texto (${parrafo}): ${largo}`);
+
+// metodos de CharAt
+
+let parrafo1 = 'hello campus'
+let donde = parrafo1.charAt(6)
+console.log(donde)
 
